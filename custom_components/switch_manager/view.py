397cfd80723b1219eb0de7c6b76ab1a940adff95
root <root@a0d7b954-ssh.local.hass.io>
2024-08-22 11:56:28 -0400

    random updates

---
+++ custom_components/switch_manager/view.py
@@ -0,0 +1,37 @@
+from .const import DOMAIN, CONF_BLUEPRINTS, BLUEPRINTS_FOLDER, PANEL_URL, NAME
+from .helpers import VERSION
+from homeassistant.core import HomeAssistant
+from homeassistant.components.frontend import async_register_built_in_panel
+
+async def async_setup_view(hass: HomeAssistant):
+
+    hass.http.register_static_path(
+        PANEL_URL,
+        hass.config.path("custom_components/switch_manager/assets/switch_manager_panel.js"),
+    )
+
+    await async_bind_blueprint_images(hass)
+
+    async_register_built_in_panel(hass,
+        component_name="custom",
+        sidebar_title=NAME,
+        sidebar_icon="mdi:light-switch-off",
+        frontend_url_path="switch_manager",
+        require_admin=True,
+        config={
+            "_panel_custom": {
+                "name": "switch-manager-panel",
+                "module_url": f"{PANEL_URL}?{VERSION}",
+                "embed_iframe": True
+            },
+            "version": VERSION
+        },
+    )
+
+async def async_bind_blueprint_images(hass: HomeAssistant):
+    for key in hass.data[DOMAIN].get(CONF_BLUEPRINTS):
+        if hass.data[DOMAIN].get(CONF_BLUEPRINTS)[key].has_image:
+            hass.http.register_static_path(
+                f'/assets/{DOMAIN}/{key}.png',
+                hass.config.path(f"{BLUEPRINTS_FOLDER}/{DOMAIN}/{key}.png"),
+            )
